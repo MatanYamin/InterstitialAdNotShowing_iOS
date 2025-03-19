3dc26d4e01714233e55046d57f749123032772bb
Matan Yamin <yamin2211@gmail.com>
2025-03-19 18:46:13 +0200

    Update AppDelegate.cs

--- TestInterstitialIOS/Platforms/iOS/AppDelegate.cs
+++ TestInterstitialIOS/Platforms/iOS/AppDelegate.cs
@@ -1,9 +1,19 @@
 ﻿using Foundation;
+using Google.MobileAds;
 
 namespace TestInterstitialIOS;
 
 [Register("AppDelegate")]
 public class AppDelegate : MauiUIApplicationDelegate
 {
-	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    protected override MauiApp CreateMauiApp()
+    {
+        var app = MauiProgram.CreateMauiApp();
+        
+        // Initialize Google AdMob
+        MobileAds.SharedInstance.Start(completionHandler: null);
+
+        return app;
+    }
+
 }
